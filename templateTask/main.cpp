11183dbf36f694b2c38827bfb6b55684242cf98a
Chris <christopher.weyand@student.hpi.de>
2017-09-28 21:00:08 +0200

    Enter commit message

---
+++ templateTask/main.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    //content
+
+
+    return 0;
+}
+

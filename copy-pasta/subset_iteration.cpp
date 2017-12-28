ade3215490782820f8b8820089f5998870045462
christopher <christopher.weyand@student.hpi.de>
2017-12-28 19:51:36 +0100

    pointer for subset iteration

--- copy-pasta/subset_iteration.cpp
+++ copy-pasta/subset_iteration.cpp
@@ -1,18 +1,18 @@
 
 template<typename T> //iterate over all subsets of size k
-void iterate_k_subsets(vector<T>& data, int k, function<void(vector<T>&)> lambda)
+void iterate_k_subsets(vector<T>& data, int k, function<void(vector<T*>&)> lambda)
 {
     assert(data.size() >= k);
     auto inside = vector<bool>(data.size(), false);
     fill(inside.begin(), inside.begin()+k, true);
-    auto subset = vector<T>;
+    auto subset = vector<T*>;
     subset.reserve(k);
     do
     {
         subset.clear();
         for(int i=0; i<inside.size(), ++i)
             if(inside[i])
-                subset.push_back[data[i]];
+                subset.push_back[&data[i]];
         lambda(subset);
     }
     while(prev_permutation(inside.begin(), inside.end()));
